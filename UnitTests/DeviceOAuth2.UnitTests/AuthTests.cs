6450bdd1b1e5dea7296f790828bd963305f012bd
Don Kackman <dkackman@gmail.com>
2015-08-31 19:47:00 -0500

    add unit tests

---
+++ UnitTests/DeviceOAuth2.UnitTests/AuthTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeviceOAuth2.UnitTests
+{
+    [TestClass]
+    public class AuthTests
+    {
+        [TestMethod]
+        public async Task FacebookAuth()
+        {
+            var keys = TestHelpers.GetAppCredentials("Facebook");
+            IDeviceOAuth2 auth = new DeviceOAuth(EndPointInfo.Facebook, (string)keys.scopes, (string)keys.client_id, (string)keys.client_secret);
+
+            auth.AuthenticatePrompt += (o, e) =>
+            {
+                TestHelpers.SpawnBrowser(e.VerificationUri, e.UserCode);
+            };
+
+            var token = await auth.Authenticate(null);
+
+            Assert.IsNotNull(token);
+            Assert.IsFalse(string.IsNullOrEmpty(token.AccessToken));
+        }
+
+        [TestMethod]
+        public async Task GoogleAuth()
+        {
+            var keys = TestHelpers.GetAppCredentials("Google");
+            IDeviceOAuth2 auth = new DeviceOAuth(EndPointInfo.Google, (string)keys.scopes, (string)keys.client_id, (string)keys.client_secret);
+
+            auth.AuthenticatePrompt += (o, e) =>
+            {
+                TestHelpers.SpawnBrowser(e.VerificationUri, e.UserCode);
+            };
+
+            var token = await auth.Authenticate(null);
+
+            Assert.IsNotNull(token);
+            Assert.IsFalse(string.IsNullOrEmpty(token.AccessToken));
+            Assert.IsFalse(string.IsNullOrEmpty(token.RefreshToken));
+        }
+    }
+}
---
+++ UnitTests/DeviceOAuth2.UnitTests/TestHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DeviceOAuth2.UnitTests
+{
+    class TestHelpers
+    {
+        public static dynamic GetAppCredentials(string name)
+        {
+            using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("DeviceOAuth2.UnitTests.keys.json")))
+            {
+                var s = reader.ReadToEnd();
+
+                var settings = new JsonSerializerSettings();
+                settings.Converters.Add(new ExpandoObjectConverter());
+
+                var keys = JsonConvert.DeserializeObject<List<dynamic>>(s, settings);
+                return keys.First(d => d.name == name);
+            }
+        }
+
+        public static void SpawnBrowser(string verificationUri, string userCode)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = string.Format("/c echo {0}| clip", userCode.Trim());
+            p.Start();
+
+            // this requires user permission - open a broswer - enter the user_code which is now in the clipboard
+            Process.Start(verificationUri);
+        }
+    }
+}
