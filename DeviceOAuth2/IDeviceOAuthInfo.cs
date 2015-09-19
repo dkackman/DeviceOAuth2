7ee79965607a9e5872f6c310eaf921fb7cd39126
Don Kackman <dkackman@gmail.com>
2015-09-19 10:39:58 -0500

    move profile and check methods to base interface

--- DeviceOAuth2/IDeviceOAuthInfo.cs
+++ DeviceOAuth2/IDeviceOAuthInfo.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading;
 using System.Threading.Tasks;
 
 namespace DeviceOAuth2
@@ -25,5 +22,39 @@ namespace DeviceOAuth2
         /// The ClientId requesting authorization
         /// </summary>
         string ClientId { get; }
+
+        /// <summary>
+        /// Checks the validity of a token against the auth endpoint.
+        /// It does this by makeing a get request to the token's <see cref="EndPointInfo.ProfileUri"/>
+        /// This is useful for ensuring that the user hasn't revoked authorization for a stored token and that it hasn't expired
+        /// </summary>
+        /// <param name="token">The token to check</param>
+        /// <returns>True if the user profile is return without error</returns>
+        Task<bool> CheckToken(TokenInfo token);
+
+        /// <summary>
+        /// Checks the validity of a token against the auth endpoint.
+        /// It does this by makeing a get request to the token's <see cref="EndPointInfo.ProfileUri"/>
+        /// This is useful for ensuring that the user hasn't revoked authorization for a stored token and that it hasn't expired
+        /// </summary>
+        /// <param name="token">The token to check</param>
+        /// <param name="cancelToken">A cancellation token</param>
+        /// <returns>True if the user profile is return without error</returns>
+        Task<bool> CheckToken(TokenInfo token, CancellationToken cancelToken);
+
+        /// <summary>
+        /// Returns the user's endpoint profile using <see cref="EndPointInfo.ProfileUri"/>
+        /// </summary>
+        /// <param name="token">The token</param>
+        /// <returns>User's profile</returns>
+        Task<dynamic> GetProfile(TokenInfo token);
+
+        /// <summary>
+        /// Returns the user's endpoint profile using <see cref="EndPointInfo.ProfileUri"/>
+        /// </summary>
+        /// <param name="token">The token</param>
+        /// <param name="cancelToken">A cancellation token</param>
+        /// <returns>User's profile</returns>
+        Task<dynamic> GetProfile(TokenInfo token, CancellationToken cancelToken);
     }
 }
